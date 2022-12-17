fb9372ec5e0539cc3aa4ba5b46c04c871bb8d50c
Andrey <amalgota@mail.ru>
2022-12-17 19:16:33 +0300

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float _speed = 3.5f;
+    void Start()
+    {
+        //take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //new Vector3(1, 0, 0); < same thing as Vector3.right
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        
+    }
+}
