feb0132298b635dd95a36ee33cd50b03c37e45ba
Andrey <amalgota@mail.ru>
2022-12-19 18:12:27 +0300

    Added laser firing logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
     
     void Start()
     {
@@ -13,32 +19,26 @@ public class Player : MonoBehaviour
         transform.position = new Vector3(0, 0, 0);
     }
 
-      void Update()
+    void Update()
     {
         CalculateMovement();
+        if (Input.GetKey(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
     }
     void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        //movement implementation #1
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
-        //movement implementation #2
+        //movement implementation
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
 
         //vertical player movement restriction, cant go above the middle on top and outside of the screen on the bottom
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
+        //Clamp implementation
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
         //horizontal player movement adjustment, when ouside of the screen (left or right) loops to the other side of the screen
         if (transform.position.x > 11)
@@ -50,5 +50,10 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(11, transform.position.y, 0);
         }
 
+    }
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
     }
 }
