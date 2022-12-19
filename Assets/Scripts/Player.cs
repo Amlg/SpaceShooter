851f28135408faa7376c98f1e270063e6baf9cbf
Andrey <amalgota@mail.ru>
2022-12-19 15:51:56 +0300

    player movement code adjustments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
     private float _speed = 3.5f;
     
-
     void Start()
     {
-        //take the current position = new position (0, 0, 0)
+        //reset initial position
         transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+      void Update()
+    {
+        CalculateMovement();
+    }
+    void CalculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-     
+
         //movement implementation #1
         //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
         //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
@@ -28,6 +29,26 @@ public class Player : MonoBehaviour
         //movement implementation #2
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        //vertical player movement restriction, cant go above the middle on top and outside of the screen on the bottom
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        //horizontal player movement adjustment, when ouside of the screen (left or right) loops to the other side of the screen
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
 
     }
 }
